03cf99514217b4f97ff12eff13989126b38cc6fa
madoka29 <tomatoice29@gmail.com>
2024-04-27 16:14:22 +0900

    プロジェクト作成

---
+++ src/main/java/com/aianalysis/AiAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.aianalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAnalysisApplication.class, args);
+	}
+
+}
